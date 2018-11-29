56a3d7e3a43f7a2f2ba495775bada67e8d97ebc1
ae.udahin <ae.udahin@tensor.ru>
2018-11-29 16:54:21 +0300

    create

--- setup.py
+++ setup.py
@@ -4,17 +4,19 @@ with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
 setup(
-    name='sealant',
-    version='1.0.0',
+    name='SeaLant',
+    version='1.0.2',
     packages=['sealant'],
     url='https://pypi.org/project/sealant',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='LICENSE.md',
     author='ae.udahin',
-    author_email='',
-    description='',
+    author_email='aejudakhin@gmail.com',
+    description='Библиотека с инструментом для поиска утечек памяти в процессе выполнения тестов в Chrome или Node.js',
     install_requires=[
-        'pychrome >= 0.2.2'
-        'requests >= 2.19.1'
+        'pychrome >= 0.2.2',
+        'requests >= 2.19.1',
         'selenium >= 3.13.0'
     ]
 )
